7c435c8a14722d44575820e5c0558ad6d3951740
arvin2k04 <153343537+arvin2k04@users.noreply.github.com>
2025-04-01 09:35:02 +0530

    Footer.jsx

--- learning-management-system-main/client/src/components/Footer.jsx
+++ learning-management-system-main/client/src/components/Footer.jsx
@@ -10,10 +10,8 @@ function Footer() {
             <footer className="bg-white py-5 lg:px-20 px-8 gap-4 w-full flex flex-col md:flex-row lg:flex-row justify-between items-center">
                 <span className='lg:text-lg md:text-lg  text-slate-600'>Copyright @{currentYear} All rights reserved</span>
                 <section className='flex items-center justify-center gap-5 text-2xl text-slate-400'>
-                    <Link to="https://www.facebook.com/sukomal.dutta.7" target='_blank' className='cursor-pointer hover:text-blue-500 transition-all ease-in-out duration-300'><BsFacebook /></Link>
-                    <Link to='https://www.linkedin.com/in/duttasukomal' target='_blank' className='cursor-pointer hover:text-blue-500 transition-all ease-in-out duration-300'><BsLinkedin /></Link>
-                    <Link to='https://github.com/Sukomal07' target='_blank' className='cursor-pointer hover:text-blue-500 transition-all ease-in-out duration-300'><BsGithub /></Link>
-                    <Link to='https://twitter.com/CryptoOrbiT6' target='_blank' className='cursor-pointer hover:text-blue-500 transition-all ease-in-out duration-300'><BsTwitter /></Link>
+                    <Link to='https://www.linkedin.com/in/baala-aravinth-sj-8b2803290' target='_blank' className='cursor-pointer hover:text-blue-500 transition-all ease-in-out duration-300'><BsLinkedin /></Link>
+                    <Link to='https://github.com/arvin2k04' target='_blank' className='cursor-pointer hover:text-blue-500 transition-all ease-in-out duration-300'><BsGithub /></Link>
                 </section>
             </footer>
         </>
